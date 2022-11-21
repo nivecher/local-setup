56442b554bc9ad92db2ef0b664c4e040d3c521bc
Morgan Davis <mtd37@hotmail.com>
2022-11-21 14:28:45 -0600

    Add support for Debian and CentOS

--- linux/base-setup.sh
+++ linux/base-setup.sh
@@ -4,6 +4,26 @@
 
 # TODO check effective user (must be run w/ sudo)
 
+function pkgmgr() {
+
+  dist=$(cat /etc/os-release | egrep "^ID=" | awk -F= '{print $2}' | tr -d "\"")
+  if [[ "$dist" == "ubuntu" ]] || [[ "$dist" == "debian" ]]; then
+    pm="apt"
+  elif [[ "$dist" == "centos" ]]; then
+    pm="dnf"
+  else
+    echo "Unsupported distribution: $dist"
+    echo "Exiting..."
+    exit 1
+  fi
+  eval $pm $@
+}
+
+# Main
+
+echo "Checking distribution"
+echo $(cat /etc/os-release)
+
 echo "Checking user"
 if [[ "$USER" != "root" ]]; then
   echo "Script must be run as root or using sudo!"
@@ -12,13 +32,23 @@ fi
 
 # Update packages
 echo "Updating packages"
-apt update -y
+pkgmgr update -y
 
-echo "Installing packages"
-apt install -y zsh git-all curl wget
+echo "Installing utility packages"
+pkgmgr install -y curl wget zsh mate tree
 
-# TODO echo "Python Setup"
+echo "Installing development packages"
+pkgmgr install -y git-all python3 python3-pip python3-virtualenv
 
+echo "Python setup"
+if [[ ! -f "/usr/bin/python" ]]; then
+  echo "Creating link to python3"
+  ln -s /usr/bin/python3 /usr/bin/python
+fi
+if [[ ! -f "/usr/bin/pip" ]]; then
+  echo "Creating link to pip3"
+  ln -s /usr/bin/pip3 /usr/bin/pip
+fi
 
 echo "Done"
 
