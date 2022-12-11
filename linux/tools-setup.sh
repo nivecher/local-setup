1dd8b2ca8794e36bf50e4baf81fc9038881fb5c5
Morgan Davis <mtd37@hotmail.com>
2022-12-11 13:21:07 -0600

    Add terraform setup

--- linux/tools-setup.sh
+++ linux/tools-setup.sh
@@ -22,6 +22,21 @@ download "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" "awscliv2.zi
 unzip awscliv2.zip
 ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
 
+echo "Installing Terraform"
+# TODO make cross-platform friendly
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+
 # TODO make configurable
 # echo "Cleaning up"
 # rm -rf temp
